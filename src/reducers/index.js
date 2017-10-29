33bced036c76a4af9d56483c80b36f60554554de
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-10-29 20:03:06 +0100

    redux state finished

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducer } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import productList from './productListReducer'
 import activeProduct from './activeProductReducer'
 import cart from './cartReducer'
 
 const rootReducer = combineReducer({
+  routing: routerReducer,
   productList,
   activeProduct,
   cart
