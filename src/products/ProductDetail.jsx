1f4a9dce953f31b5da4b6ffb159d62a25dd82a90
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-11-14 11:32:20 +0100

    shopping detail

---
+++ src/products/ProductDetail.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductDetail = ({ loading, product, onAddItem }) => {
+  if (!loading && product) {
+    const { _id, image, name, description, price, deliveryState, category } = product
+
+    return (
+      <section className='container'>
+        <div className='row'>
+          <figure className='figure col-lg-6'>
+            <img className='figure-img img-thumbnail rounded' src={image} alt={description} />
+          </figure>
+          <div className='col-lg-6'>
+            <br />
+            <h4>{ name }</h4>
+            <p>{ description }</p>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                Precio: <strong> { price } &euro; </strong>
+              </li>
+              <li className='list-group-item'>
+                Entrega: <strong> { deliveryState } </strong>
+              </li>
+              <li className='list-group-item'>
+                Categoría: <span className='bagde badge-pill badge-info'>{ category }</span>
+              </li>
+            </ul>
+            <br />
+            <button className='btn btn-primary' onClick={() => onAddItem(product)}>
+              <span className='fa fa-cart-plus' />Añadir al carrito
+            </button>
+          </div>
+        </div>
+      </section>
+    )
+  }
+  return (<span>Cargando Datos...</span>)
+}
+
+ProductDetail.propTypes = {
+  product: PropTypes.object,
+  loading: PropTypes.bool.isRequired
+}
+
+export default ProductDetail
